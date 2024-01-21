487ef3e251d685b5e870f82400008a317e54794b
praneeth <praneeth@mit.edu>
2024-01-21 11:30:30 -0500

    Documentation upgrade

--- README.md
+++ README.md
@@ -14,12 +14,14 @@ pip install pyfilemanager
 ### Quickstart
 
 ```python
-from pyFileManager import FileManager
-video_list = FileManager(r'C:\videos').add('video', '*.avi', include=['sony', 'panasonic'], exclude='canon')['video']
+from pyfilemanager import FileManager
+fm = FileManager(r'C:\videos')
+fm.add('canon', '*Camera.avi', include='canon')
+video_list = fm['video']
 ```
 
 ### Long version
-Consider the following file structure in C:\videos directory
+Consider the following directory structure
 
 ```
 C:\videos
@@ -28,11 +30,11 @@ C:\videos
 │   ├── 143Camera.avi
 │   ├── notes.txt
 ├── panasonic
-│   ├── 151camera.avi
-│   ├── 143camera.avi
+│   ├── 151Camera.avi
+│   ├── 143Camera.avi
 │   ├── notes.txt
 ├── panasonic2
-│   ├── 201camera.avi
+│   ├── 201Camera.avi
 │   ├── 202.mp4
 ├── canon
 │   ├── 51Camera.avi
@@ -45,21 +47,53 @@ C:\videos
 
 1. Initialize the file manager.
    ```python
-   fm = FileManager(r'C:\videos')
+   fm = FileManager(r'C:\videos', exclude_hidden=True)
    ```
-2. Add files of different types with different inclusion and exclusion criteria.
+2. Add files based on different inclusion and exclusion criteria. Use the include parameter to keep file paths that contain **all** of the supplied strings *anywhere* in the file path. Use the exclude parameter to disregard file paths that contain **any** of the supplied string anywhere in the file path.
    ```python
-    fm.add('video', '*Camera.avi', include=['sony', 'panasonic'], exclude='canon')
-    fm.add('video', '*.mp4', include='panasonic') # to include 202.mp4 in the panasonic2 folder
     fm.add('canon', '*Camera.avi', include='canon')
+    # file path must contain canon
+
+    fm.add('sony42', '*Camera.avi', include=['sony', '42'])
+    # file path must contain sony AND 42
+    # grabs one file - sony\142Camera.avi
+    
+    fm.add('videos', ['*.avi', '*.mp4'])
+    # add multiple conditions by supplying them as a list or tuple
+    
+    fm.add('panasonic', '*.avi', include='panasonic', exclude='panasonic2')
+    # grab videos in the panasonic folder
+    
     fm.add('notes', 'notes*.txt')
+    # grab all notes
+
+    fm.add('notes', 'notes*.txt', include='notes\\notes')
+    # grab notes from the notes folder
+    # this will overwrite the previous notes entry
+
+    fm.add('notes', 'notes*.txt', exclude=['sony', 'panasonic', 'panasonic2', 'canon'])
+    # achieves the same result as the previous line
     ```
-3. Retrieve the file list.
+3. Retrieve file paths using a dict-like convention.
    ```python
     canon_videos = fm['canon']
-    other_videos = fm['video']
+    all_videos = fm['videos']
     ```
-4. Generate a report for the different file types (number of files, and space occupied).
+4. Add and retrieve in one line of code. 
+   ```python
+   fm.add('canon', '*Camera.avi', include='canon')['canon']
+   # Note that the method returns the instance of the FileManager object. 
+   ```
+5. Retrieve all the added keys using fm.**get_tags**()
+   ```python
+   fm.get_tags()
+   # returns a tuple ('canon', 'sony42', 'videos', 'panasonic', 'notes')
+   ```
+6. Retrieve paths of all the added files.
+   ```python
+   fm.all_files
+   ```
+7. Get a report of the number of files, and the occupied space.
    ```python
     fm.report()
     ```
--- pyfilemanager/__init__.py
+++ pyfilemanager/__init__.py
@@ -2,50 +2,39 @@ import os
 import fnmatch
 from pathlib import Path
 
+from typing import Union
+
 class FileManager:
     """
-    Easy to use file search and file path management. Useful for managing files in not-so-obviously organized folders.
+    Easy to use file search and file path management.
+    Add files using different inclusion and exclusion criteria under a 'tag'.
+    Provides dictionary-like access to file-paths where the tags serve as keys.
+    Useful for managing files in not-so-obviously organized folders.
 
-    Usage: 
-        TL;DR
-        from pyFileManager import FileManager
-        video_list = FileManager(r'C:\videos').add('video', '*.avi', include=['sony', 'panasonic'], exclude='canon')['video']
+    Args:
+        base_dir (str): base directory for file search
+        exclude_hidden (bool, optional): excludes hidden files when True. Defaults to True.
 
-        Long version
-        Consider the following file structure in C:\videos directory
-        C:\videos
-        -- sony
-           -- 142Camera.avi
-           -- 143Camera.avi
-           -- notes.txt
-        -- panasonic
-           -- 151Camera.avi
-           -- 152Camera.avi
-           -- notes.txt
-        -- panasonic2
-           -- 201Camera.avi
-           -- 202.mp4
-        -- canon
-           -- 51Camera.avi
-           -- 40Camera.avi
-           -- notes.txt
-        -- notes
-           -- notes1.txt
-           -- notes2.txt
-        1. Initialize the file manager
-            fm = FileManager(r'C:\videos')
-        2. Add files of different types with different inclusion and exclusion criteria
-            fm.add('video', '*Camera.avi', include=['sony', 'panasonic'], exclude='canon')
-            fm.add('video', '*.mp4', include='panasonic') # to include 202.mp4 in the panasonic2 folder
-            fm.add('canon', '*Camera.avi', include='canon')
-            fm.add('notes', 'notes*.txt')
-        3. Retrieve the file list
-            canon_videos = fm['canon']
-            other_videos = fm['video']
-        4. Generate a report for the different file types (number of files, and space occupied)
-            fm.report()
-    """
-    def __init__(self, base_dir, exclude_hidden=True):
+    Attributes:
+        base_dir (str): base directory for file search
+        all_files (list): Return a list of all file paths managed by the filemanager. Remove duplicates.
+
+        _files (dict): {Tag: List of file paths}
+        _filters (dict): {Tag: pattern list}
+        _inclusions (dict): {Tag: inclusion criteria}
+        _exclusions (dict): {Tag: exclusion criteria}
+        
+    Methods:
+        add: Add files based on different inclusion and exclusion criteria.
+        get_tags: Return a list of tags created using the add method.
+        report: Print a report summarizing the size occupied by files under each tag.
+        remove: Remove file paths stored under a given tag. May not be very useful.
+        __getitem__: overloaded.
+
+        _find: Core method for finding files based on os.walk and fnmatch.
+        _include, _exclude: Utilities used by the add method.
+    """    
+    def __init__(self, base_dir: str, exclude_hidden: bool=True):
         assert isinstance(base_dir, str)
         self.base_dir = os.path.realpath(base_dir)
         self._files = {}
@@ -55,90 +44,169 @@ class FileManager:
         assert isinstance(exclude_hidden, bool)
         self._exclude_hidden = exclude_hidden
     
-    def add(self, type_name, pattern_list, include=None, exclude=None, exclude_hidden=None):
-        """
-        Add a type of file with a given filter.
-        e.g. fm.add('video', '*Camera*.avi')
+    def add(self, tag: str, pattern_list: Union[str,list], include: Union[str,list]=None, exclude: Union[str,list]=None, exclude_hidden: bool=None) -> FileManager:
+        """Add files based on different inclusion and exclusion criteria.
+
+        Example:
+            fm.add('video', '*Camera*.avi')
+
+        Args:
+            tag (str): e.g. 'video_files'
+            pattern_list (Union[str,list]): e.g. '*.avi', ['*.avi', '*.mp4']
+            include (Union[str,list], optional): Keep file paths that contain **all** of the supplied strings *anywhere* in the file path. Defaults to None.
+            exclude (Union[str,list], optional): Disregard file paths that contain **any** of the supplied string anywhere in the file path. Defaults to None.
+            exclude_hidden (bool, optional): Set the state for excluding hidden files. Defaults to the value of _exclude_hidden attribute, which defaults to True.
+
+        Returns:
+            FileManager: Eeturns self. Useful for chaining commands.
         """
         if exclude_hidden is None: # None means not specified. In this case, set it to the global default.
             exclude_hidden = self._exclude_hidden
+        
         if isinstance(pattern_list, str):
             pattern_list = [pattern_list]
-        self._files[type_name] = []
+        
+        self._files[tag] = []
+        
         for pattern in pattern_list:
-            self._files[type_name] += self._find(pattern, path=self.base_dir, exclude_hidden=exclude_hidden)
-        self._filters[type_name] = pattern_list
-        self._inclusions[type_name] = []
-        self._exclusions[type_name] = []
+            self._files[tag] += self._find(pattern, path=self.base_dir, exclude_hidden=exclude_hidden)
+        
+        self._filters[tag] = pattern_list
+        self._inclusions[tag] = []
+        self._exclusions[tag] = []
 
         if include is not None:
             if isinstance(include, str):
-                self._include(type_name, include)
+                self._include(tag, include)
             else:
                 assert isinstance(include, (list, tuple))
                 for inc_str in include:
                     assert isinstance(inc_str, str)
-                    self._include(type_name, inc_str)
+                    self._include(tag, inc_str)
         
         if exclude is not None:
             if isinstance(exclude, str):
-                self._exclude(type_name, exclude)
+                self._exclude(tag, exclude)
             else:
                 assert isinstance(exclude, (list, tuple))
                 for exc_str in exclude:
                     assert isinstance(exc_str, str)
-                    self._exclude(type_name, exc_str)
+                    self._exclude(tag, exc_str)
         
         return self # for chaining commands
+    
+    def remove(self, tag: str) -> None:
+        """Remove file paths stored under the given tag.
 
-    def _include(self, type_name, inclusion_string):
+        Args:
+            tag (str): A tag created when using the add method.
+
+        Raises:
+            ValueError: If an unknown tag is supplied.
         """
-        Include a set of files from the list. Useful for choosing files in specific sub-folders.
+        if tag in self._files:
+            del self._files[tag]
+        else:
+            raise ValueError(f'Unknown type {tag}')
+
+    def _include(self, tag: str, inclusion_string: str) -> None:
+        """Include a set of files from the list. Useful for choosing files in specific sub-folders.
         Use this functionality using the add method.
-        """
-        assert type_name in self._files
-        self._files[type_name] = [fn for fn in self._files[type_name] if inclusion_string in fn]
-        self._inclusions[type_name].append(inclusion_string)
 
-    def _exclude(self, type_name, exclusion_string):
-        """
-        Exclude a set of files from the list. Useful for ignoring files in specific sub-folders.
+        Args:
+            tag (str): A tag created when using the add method.
+            inclusion_string (str): File paths containing this string anywhere in the path will be kept.
+        """        
+        assert tag in self._files
+        self._files[tag] = [fn for fn in self._files[tag] if inclusion_string in fn]
+        self._inclusions[tag].append(inclusion_string)
+
+    def _exclude(self, tag: str, exclusion_string: str):
+        """Exclude a set of files from the list. Useful for ignoring files in specific sub-folders.
         Use this functionality using the add method.
-        """
-        assert type_name in self._files
-        self._files[type_name] = [fn for fn in self._files[type_name] if exclusion_string not in fn]
-        self._exclusions[type_name].append(exclusion_string)
 
-    def __getitem__(self, key):
+        Args:
+            tag (str): A tag created when using the add method.
+            exclusion_string (str): File paths containing this string anywhere in the path will be removed.
+        """
+        assert tag in self._files
+        self._files[tag] = [fn for fn in self._files[tag] if exclusion_string not in fn]
+        self._exclusions[tag].append(exclusion_string)
+
+    def __getitem__(self, key: str) -> list:
+        """Retrieve file paths based on - 
+            (1) tag
+            (2) exact match for the 'stem' of the file
+            (3) key is anywhere in the path 
+            Try (2) only if (1) doesn't return any results, and try (3) only if (2) doesn't return any results.
+
+        Args:
+            key (str): Either a tag, filename, or partial match.
+
+        Returns:
+            list: List of file paths.
+        """        
+        # (1) by tag
         if key in self._files:
             return self._files[key]
+        
+        # (2) full-stem search
         all_files = self.all_files
-        stem_to_path = {Path(x).stem:x for x in all_files}
-        # full-stem search
+        stem_to_path = {Path(x).stem:[] for x in all_files} # in case there are multiiple files with the same 'stem'
+        for file_name in all_files:
+            stem_to_path[Path(file_name).stem].append(file_name)
         if key in stem_to_path:
             return stem_to_path[key]
-        # loose search - full path contains
-        return [x for x in all_files if key in x]
+        
+        # (3) loose search - full path contains
+        return self._unique_sorted([x for x in all_files if key in x])
         
     
-    def types(self):
-        return self._files.keys()
+    def get_tags(self) -> list:
+        """Return a list of tags created using the add method.
+
+        Returns:
+            list: List of tags.
+        """        
+        return list(self._files.keys())
 
     @property
-    def all_files(self):
-        ret = []
-        for ftype in self.types():
-            ret += self[ftype]
-        return ret
+    def all_files(self) -> list:
+        """Return a list of all files managed by the filemanager. Remove duplicates.
 
-    def report(self, units='MB'):
+        Returns:
+            list: List of file paths
+        """
+        ret = []
+        for ftype in self.get_tags():
+            ret += self[ftype]
+        return self._unique_sorted(ret)
+
+    def report(self, units: str='MB') -> None:
+        """Print a report summarizing the size occupied by files under each tag.
+
+        Args:
+            units (str, optional): One of ('B', 'KB', 'MB', 'GB', 'TB). B is for bytes. Defaults to 'MB'.
+        """        
         for file_type, file_list in self._files.items():
             fs = sum(list(self._file_size(file_list, units=units).values()))
             print(str(len(file_list)) + ' ' + file_type + ' files taking up {:4.3f} '.format(fs) + units)
     
     @staticmethod
-    def _find(pattern, path=None, exclude_hidden=True):
-        "Example: find('*.txt', '/path/to/dir')"
+    def _find(pattern: str, path: str=None, exclude_hidden: bool=True) -> list:
+        """Core method for finding files based on os.walk and fnmatch.
+
+        Example:
+            find('*.txt', '/path/to/dir')
+
+        Args:
+            pattern (str): Input for fnmatch.
+            path (str, optional): Search for files in this path. Defaults to the results of os.getcwd().
+            exclude_hidden (bool, optional): Whether to include filenames of hidden files. Defaults to True.
+
+        Returns:
+            list: List of file names.
+        """
         if path is None:
             path = os.getcwd()
         
@@ -154,9 +222,15 @@ class FileManager:
         return result
     
     @staticmethod
-    def _file_size(file_list, units='MB'):
-        """
-        Returns files sizes in descending order (default: megabytes)
+    def _file_size(file_list: list, units: str='MB') -> dict:
+        """Returns files sizes in descending order (default: megabytes). Used by the report method.
+
+        Args:
+            file_list (list): list of file names
+            units (str, optional): One of ('B', 'KB', 'MB', 'GB', 'TB). B is for bytes. Defaults to 'MB'.
+
+        Returns:
+            dict: {file_name : size}
         """
         div = {'B':1, 'KB':1024, 'MB':1024**2, 'GB':1024**3, 'TB':1024**4}
         if isinstance(file_list, str):
@@ -166,4 +240,18 @@ class FileManager:
         size_list = list(size_mb.keys())
         size_list.sort(reverse=True)
         return {size_mb[s]:s for s in size_list} # {file_name : size}
+    
+    @staticmethod
+    def _unique_sorted(file_list: list) -> list:
+        """Utility to return a sorted list of unique items in the input list.
+
+        Args:
+            file_list (list): Expected to be a list of strings (file names).
+
+        Returns:
+            list: List of unique elements (file names).
+        """        
+        ret = list(set(file_list))
+        ret.sort()
+        return ret
     
